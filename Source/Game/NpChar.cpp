2ea263e9764362ca9a897b604d45fa2d75620b3a
hax0kartik <agarwala.kartik@gmail.com>
2023-01-20 19:08:04 +0530

    Fix indentation

--- Source/Game/NpChar.cpp
+++ Source/Game/NpChar.cpp
@@ -11,12 +11,12 @@ void InitNpChar(NpChar *npc)
         npc[i].ani_no = 0;
         npc[i].ani_wait = 0;
         npc[i].direct = 0;
-		npc[i].x = 0;
-		npc[i].y = 0;
-		npc[i].xm = 0;
-		npc[i].ym = 0;
-		npc[i].tgt_x = 0;
-		npc[i].tgt_y = 0;
+        npc[i].x = 0;
+        npc[i].y = 0;
+        npc[i].xm = 0;
+        npc[i].ym = 0;
+        npc[i].tgt_x = 0;
+        npc[i].tgt_y = 0;
         npc[i].cond = false;
     }
 }
